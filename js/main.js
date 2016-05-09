13f19cb62025f9fa5ed49d70380441842a24f0f2
retwedt <retwedt@gmail.com>
2016-05-08 20:12:33 -0500

    Update 2016-05-09T01:12:20.519Z

---
+++ js/main.js
@@ -0,0 +1,2 @@
+console.log("this is main.js!");var nav=document.querySelector(".navbar");document.onscroll=function(){document.body.scrollTop<=40?(nav.classList.add("navbar-top"),nav.classList.remove("navbar-scroll")):(nav.classList.add("navbar-scroll"),nav.classList.remove("navbar-top"))};
+//# sourceMappingURL=maps/main.js.map
